75e669d58e6207cb2b5ec8220071a78cc363095a
spiralbaba <rohitkumar303.hitece2020@gmail.com>
2026-07-16 05:09:42 +0530

    Changed the server.js code for hosting

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const server = http.createServer((req, res) => {
 
 });
 
-server.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
